9e67a7cc20d6c3ee42df27cde293b8798fb880d7
Samuele <samueleiac@gmail.com>
2021-02-24 17:48:07 +0100

    inizio project

--- src/Model/Project.java
+++ src/Model/Project.java
@@ -1,12 +1,23 @@
 package Model;
 
+import java.util.ArrayList;
+
 /**
  * classe che gestisce i progetti
  */
 public class Project {
     private String name;
+    private ArrayList<String> TODO;
+    private ArrayList<String> INPROGRESS;
+    private ArrayList<String> TOBEREVISITED;
+    private ArrayList<String> DONE;
+
 
     public Project(String name) {
         this.name = name;
+        this.TODO = new ArrayList<>();
+        this.INPROGRESS = new ArrayList<>();
+        this.TOBEREVISITED = new ArrayList<>();
+        this.DONE = new ArrayList<>();
     }
 }
